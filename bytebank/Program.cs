8eb3887fd197047b18a0a02264765f58b39d6b63
jonasRamosz <jrenovaveis@gmail.com>
2022-08-15 09:54:13 -0300

    classes do bytebank

---
+++ bytebank/Program.cs
@@ -0,0 +1,22 @@
+﻿using bytebank;
+
+
+
+
+
+//contaCorrente jonas = new contaCorrente("Agencia Sampaio", 415, "4577");
+//jonas.titular = new Cliente();
+//jonas.titular.nome = "jonas ramos";
+//jonas.titular.cpf = "45800";
+//jonas.titular.profissao = "programador";
+//jonas.Saldo = 15000;
+//Console.WriteLine($"nome :{jonas.titular.nome} \ncpf :{jonas.titular.cpf} \nprofissão :{jonas.titular.profissao}" +
+//    $"\nnome da agencia :{jonas.nomeAgencia} \nnumero da agencia :{jonas.numeroAgencia} \nconta :{jonas.conta}" +
+//    $"\nsaldo :{jonas.Saldo}");
+
+MontaTela tela = new MontaTela();
+tela.TelaInicial();
+Console.Write("Digite... ");
+int n1 = Convert.ToInt16(Console.ReadLine());
+string oi = Console.ReadLine();
+Console.WriteLine($"vc digitou {oi}");
---
+++ bytebank/contaCorrente.cs
@@ -0,0 +1,127 @@
+﻿using bytebank;
+namespace bytebank
+{
+    public class contaCorrente
+{
+    public contaCorrente( string nome_agencia, int numero_agencia, string contas)
+        {
+           
+            nomeAgencia = nome_agencia;
+            numeroAgencia = numero_agencia;
+            conta = contas;
+
+            totalDeContasCriadas += 1;
+
+        }
+        public static int totalDeContasCriadas { get; set; }
+
+        public Cliente titular { get; set; }
+    private string _conta;
+    public string conta
+        {
+            get 
+            {
+                return _conta; 
+            }
+            set
+            {
+                if (value == null) 
+                {
+                    return;
+                }
+                else
+                {
+                    _conta = value;
+                }
+            }
+        }
+    
+    private int _numeroAgencia;
+    public int numeroAgencia 
+        {
+            get 
+            {
+                return _numeroAgencia; 
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _numeroAgencia = value;
+                }
+            }
+
+        }
+    public string nomeAgencia { get; set; }
+
+    private double saldo;
+    
+  
+
+
+
+    public bool sacar(double valor)
+    {
+        if (valor < 0)
+        {
+            return false;
+        }
+        if (saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            saldo -= valor;
+            return true;
+        }
+    }
+
+    public void depositar(double valor)
+    {
+        saldo += valor;
+    }
+
+    public bool transfirir(double valor, contaCorrente destino)
+    {
+        if (saldo < valor)
+        {
+            return false;
+        }
+        if (valor < 0)
+        {
+            return false;
+        }
+        else
+        {
+            saldo -= valor;
+            destino.saldo += valor;
+            return true;
+        }
+    }
+
+    public double Saldo
+        {
+            get { return saldo; }
+            
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                saldo = value;
+               
+            }
+        }
+    
+    
+    }
+  
+
+
+}
